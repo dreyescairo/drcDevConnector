04796439225beab984f03d264e1fb512fe09ec76
David Reyes-Cairo <dreyescairo@gmail.com>
2018-07-19 01:10:35 -0600

    trying to get mongoose running. lesson #7

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 //require the express module
-const express = require('express');
-
-
+const express = require("express");
+const mongoose = require("mongoose");
 //init our express app
 const app = express();
 
+//dbconfig
+const db = require("./config/keys").mongoURI;
 
-app.get('/', (req, res) => res.send('Hello'));
+//connect to mongodb
+
+mongoose
+  .connect(db)
+  .then(() => console.log("mongoDB connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World (How Cliche')"));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server running on port ${port}`));
