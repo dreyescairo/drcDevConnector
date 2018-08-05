83ee8db37ffc22a9e103b2de075d178c7a99f2e1
David Reyes-Cairo <dreyescairo@gmail.com>
2018-08-04 22:48:16 -0600

    Gotta finish the posts routes up.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -9,6 +9,7 @@ const passport = require('passport');
 
 //models
 const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
 
 //validation
 const postValidation = require('../../validation/postValidation');
@@ -48,5 +49,56 @@ router.post('/', passport.authenticate('jwt', {
   newPost.save().then(post => res.json(post));
 
 });
+
+//@route DELETE api/posts/:id
+//@desc delete the post
+//@access private
+router.delete('/:id', passport.authenticate('jwt', {
+  session: false
+}), (req, res) => {
+
+
+  Profile.findOne({
+    user: req.user.id
+  }).then(profile => {
+    Post.findById(req.params.id).then(post => {
+      //check for post owner
+      if (post.user.toString() !== req.user.id) {
+        return res.status(401).json({
+          notAuthorized: "User not authorized"
+        });
+      } else {
+        post.remove().then(() => res.json({
+          success: true
+        })).catch(err => res.status(404).json({
+          postNotFound: "The post you are looking for is not found!"
+        }));
+      }
+    });
+  });
+
+
+});
+
+//@route GET api/posts
+//@desc get posts
+//@access public
+router.get('/', (req, res) => {
+  Post.find().sort({
+    date: -1
+  }).then(posts => res.json(posts)).catch(err => res.status(404).json({
+    errorFindingPosts: "No posts found"
+  }));
+});
+
+//@route GET api/posts/:id
+//@desc get posts by id
+//@access public
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id).then(post => res.json(post)).catch(err => res.status(404).json({
+    errorFindingPostByID: "There was a problem finding the post!"
+  }));
+});
+
 
 module.exports = router;
